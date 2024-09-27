2185985d79c6376cc878f2b2322a7a461daa86a5
rishi chaturvedi <chaturvedi0718@gmail.com>
2024-09-27 15:38:49 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,29 +1,22 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
-        roman_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
+Here is the code:
 
-        total = 0
 
-        
-        for i in range(len(s)):
-            
-            if i + 1 < len(s) and roman_dict[s[i]] < roman_dict[s[i + 1]]:
-                total -= roman_dict[s[i]]
-            else:
+
+def isValid(s: str) -> bool:
+    bracket_map = {")": "(", "}": "{", "]": "["}
+    stack = []
+    
+    for char in s:
+        if char in bracket_map.values():
+            stack.append(char)
+        elif char in bracket_map.keys():
+            if not stack or stack.pop() != bracket_map[char]:
+                return False
                 
-                total += roman_dict[s[i]]
+    return not stack
 
-    returntotal
+
+# Example usage:
+print(isValid("()"))  
+print(isValid("()[]{}"))  
+print(isValid("(]"))  
