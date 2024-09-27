4ed0637104dbae4547a7b5364d0d603578fab6f6
rishi chaturvedi <chaturvedi0718@gmail.com>
2024-09-27 15:21:50 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -26,30 +26,4 @@ class Solution(object):
                 
                 total += roman_dict[s[i]]
 
-        return total
-
-
-def isValid(s: str) -> bool:
-    
-    bracket_map = {')': '(', '}': '{', ']': '['}
-
-    
-    stack = []
-
-    
-    for char in s:
-        if char in bracket_map:
-            
-            top_element = stack.pop() if stack else '#'
-
-            
-            if bracket_map[char] != top_element:
-                return False
-        else:
-            
-            stack.append(char)
-
-    
-    return not stack
-
-
+        return total
