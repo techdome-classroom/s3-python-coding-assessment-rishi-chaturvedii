038ca243b1fe114a65b8d38b3c58dc889e89da42
rishi chaturvedi <chaturvedi0718@gmail.com>
2024-09-27 15:18:23 +0530

    Auto commit on save

---
+++ program1.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
+
+        stack = []
+        
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+    
+        return not stack
